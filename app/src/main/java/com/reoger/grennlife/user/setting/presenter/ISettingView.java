9289cf8ba1acdb7aa21548ea406a3984656afe17
Jack <naaturalmaster@gmail.com>
2016-10-20 22:18:17 +0800

    finish setting view

--- app/src/main/java/com/reoger/grennlife/user/setting/presenter/ISettingView.java
+++ app/src/main/java/com/reoger/grennlife/user/setting/presenter/ISettingView.java
@@ -1,10 +1,12 @@
 package com.reoger.grennlife.user.setting.presenter;
 
 import android.app.Activity;
+import android.content.Context;
 
 /**
  * Created by Zimmerman on 2016/10/19.
  */
 public interface ISettingView {
     void logOut();
+    void doAppShare(Context mContext);
 }
--- app/src/main/java/com/reoger/grennlife/user/setting/view/SettingActivity.java
+++ app/src/main/java/com/reoger/grennlife/user/setting/view/SettingActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.reoger.grennlife.R;
@@ -16,8 +17,13 @@ import com.reoger.grennlife.user.setting.presenter.SettingCompl;
  */
 public class SettingActivity extends AppCompatActivity implements View.OnClickListener{
     private ISettingView mSettingCompl;
+
     private LinearLayout mExitLayout;
     private LinearLayout mContactUsLayout;
+    private LinearLayout mSharedLayout;
+    private LinearLayout mPrivacyLayout;
+
+    private ImageButton mToolBarBackBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,25 +34,42 @@ public class SettingActivity extends AppCompatActivity implements View.OnClickLi
 
     private void init() {
         mSettingCompl = new SettingCompl(this);
+        //返回按钮
+        mToolBarBackBtn = (ImageButton) findViewById(R.id.toolbar_button_return);
         //设置按钮条
         mExitLayout = (LinearLayout) findViewById(R.id.setting_layout_logout);
         //联系我们按钮条
         mContactUsLayout = (LinearLayout) findViewById(R.id.setting_layout_contact_us);
+        //应用分享按钮条
+        mSharedLayout = (LinearLayout) findViewById(R.id.setting_layout_share);
+        //隐私条款按钮条
+        mPrivacyLayout = (LinearLayout) findViewById(R.id.setting_layout_privacy);
 
+        mPrivacyLayout.setOnClickListener(this);
         mContactUsLayout.setOnClickListener(this);
+        mSharedLayout.setOnClickListener(this);
         mExitLayout.setOnClickListener(this);
+        mToolBarBackBtn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.toolbar_button_return:
+                this.finish();
+                break;
             case R.id.setting_layout_logout:
                 mSettingCompl.logOut();
                 break;
             case R.id.setting_layout_contact_us:
                 startActivity(new Intent(this,ContactUsView.class));
                 break;
-
+            case R.id.setting_layout_share:
+                mSettingCompl.doAppShare(this);
+                break;
+            case R.id.setting_layout_privacy:
+                startActivity(new Intent(this,PrivacyActivity.class));
+                break;
         }
     }
 }
